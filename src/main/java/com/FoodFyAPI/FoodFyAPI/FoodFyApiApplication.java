7c61f3398d5ceea270dd28d11eab0b1052116847
Jose <161727897+joseaej@users.noreply.github.com>
2025-04-08 16:29:56 +0200

    created project

---
+++ src/main/java/com/FoodFyAPI/FoodFyAPI/FoodFyApiApplication.java
@@ -0,0 +1,13 @@
+package com.FoodFyAPI.FoodFyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FoodFyAPI/FoodFyAPI/FoodFyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FoodFyAPI.FoodFyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
